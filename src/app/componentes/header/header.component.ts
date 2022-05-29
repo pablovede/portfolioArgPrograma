eab044444e9550822b0b9f7406f518ff256f4b93
pablovede <pabloved2@gmail.com>
2022-05-29 02:29:23 -0300

    agregado el deploy + conexion a Heroku

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -9,7 +9,10 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  irAInstagram() {
+    
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://stormy-citadel-14002.herokuapp.com'
 };
